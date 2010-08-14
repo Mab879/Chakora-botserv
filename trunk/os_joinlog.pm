62657e82d9aa03bd41861bd39ea513fe6bc592c8
Matthew Barksdale <mattwb65@gmail.com>
2010-08-14 14:11:59 -0400

    Updated to support new module api

--- trunk/os_joinlog.pm
+++ trunk/os_joinlog.pm
@@ -6,10 +6,16 @@
 #
 # Copyright (c) 2010 The Chakora Project. All rights reserved.
 # Released under The BSD License (docs/LICENSE - http://www.opensource.org/licenses/bsd-license.php)
-module_init("operserv/joinlog", "The Chakora Project", "0.1", \&init_os_joinlog, "all");
+module_init("operserv/joinlog", "The Chakora Project", "0.1", \&init_os_joinlog, \&void_os_joinlog, "all");
 
 sub init_os_joinlog {
 	hook_join_add(\&svs_os_joinlog);
+}
+
+sub void_os_joinlog {
+	delete_sub 'init_os_joinlog';
+	delete_sub 'svs_os_joinlog';
+	hook_join_del(\&svs_os_joinlog);	
 }
 
 sub svs_os_joinlog {
