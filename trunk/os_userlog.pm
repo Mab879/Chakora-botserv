18c2801097911c6d2074ce70d969c197300822c8
Matthew Barksdale <mattwb65@gmail.com>
2010-08-15 08:00:53 -0400

    Added NICK tracking

--- trunk/os_userlog.pm
+++ trunk/os_userlog.pm
@@ -11,22 +11,30 @@ module_init("operserv/userlog", "The Chakora Project", "0.1", \&init_os_userlog,
 sub init_os_userlog {
 	hook_join_add(\&svs_os_joinlog);
 	hook_part_add(\&svs_os_partlog);
+	hook_nick_add(\&svs_os_nicklog);
 }
 
 sub void_os_userlog {
 	delete_sub 'init_os_userlog';
 	delete_sub 'svs_os_joinlog';
-	delete_sub 'svs_os_partlog;
+	delete_sub 'svs_os_partlog';
+	delete_sub 'svs_os_nicklog';
 	hook_join_del(\&svs_os_joinlog);	
 	hook_part_del(\&svs_os_partlog);
+	hook_nick_del(\&svs_os_nicklog);
 }
 
 sub svs_os_joinlog {
         my ($user, $chan) = @_;
-        serv_privmsg("os", config('log', 'logchan'), "JOIN: ".uidInfo($user, 1)." -> ".$chan);
+        serv_privmsg("os", config('log', 'logchan'), "\2JOIN\2: ".uidInfo($user, 1)." -> ".$chan);
 }
 
 sub svs_os_partlog {
 	my ($user, $chan) = @_;
-	serv_privmsg("os", config('log', 'logchan'), "PART: ".uidInfo($user, 1)." -> ".$chan);
+	serv_privmsg("os", config('log', 'logchan'), "\2PART\2: ".uidInfo($user, 1)." -> ".$chan);
+}
+
+sub svs_os_nicklog {
+	my ($user, $newnick) = @_;
+	serv_privmsg("os", config('log', 'logchan'), "\2NICK\2: ".uidInfo($user, 6)." -> ".$newnick);
 }
