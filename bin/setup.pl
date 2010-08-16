ec5ff59613427ed1fb10ea3af32d6ed6f57f1444
Matthew Barksdale <mattwb65@gmail.com>
2010-08-16 07:45:10 -0400

    We need to have single quotes around the key

--- bin/setup.pl
+++ bin/setup.pl
@@ -95,7 +95,8 @@ $conf->append("\tautoload = \"\"\n}\n\n");
 
 $conf->append("encryption {\n");
 my $key = prompt('x', 'What do you want to use for the encryption key', 'This needs to be unique to your IRC network, make sure this strong and random', '');
-$conf->append("\tkey = ".$key."\n}\n");
+$conf->append("\t"."key = '".$key."'\n");
+$conf->append("}\n");
 
 my $svs = 'ChanServ';
 $conf->append(lc($svs)." {\n");
