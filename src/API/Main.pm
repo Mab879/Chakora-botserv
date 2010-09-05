47ee2292d780e0a1c500e12e55c6a168eeac3200
Elijah Perrault <elijah@technoirc.org>
2010-09-04 19:59:51 -0600

    Use public variables from the package.

--- src/API/Main.pm
+++ src/API/Main.pm
@@ -20,10 +20,10 @@ sub module_init {
             &{ $init_handler }();
             print("[MODULES] ".$name.": Module successfully loaded.\n");
             svsflog("chakora", "[MODULES] ".$name.": Module successfully loaded.");
-            $MODULE{$name}{name} = $name;
-            $MODULE{$name}{author} = $author;
-            $MODULE{$name}{version} = $version;
-            $MODULE{$name}{void} = $void_handler;
+            $Chakora::MODULE{$name}{name} = $name;
+            $Chakora::MODULE{$name}{author} = $author;
+            $Chakora::MODULE{$name}{version} = $version;
+            $Chakora::MODULE{$name}{void} = $void_handler;
             return "MODLOAD_SUCCESS";
             1;	
         } or print("[MODULES] ".$name.": Module failed to load. $@\n") and svsflog("chakora", "[MODULES] ".$name.": Module failed to load.") and return "MODLOAD_FAIL";
@@ -31,10 +31,10 @@ sub module_init {
 }
 
 sub module_exists {
-	my ($module) = @_;
+	my ($Chakora::MODULE) = @_;
 	my $exists = 0;
-	foreach my $mod (keys %MODULE) {
-		if ($mod eq $module) {
+	foreach my $mod (keys %Chakora::MODULE) {
+		if ($mod eq $Chakora::MODULE) {
 			$exists = 1;
 		}
 	}
@@ -42,23 +42,23 @@ sub module_exists {
 }
 
 sub module_void {
-	my ($module) = @_;
-	svsflog("chakora", "[MODULES] ".$module.": Attempting to unload module. . .");
-	if (defined($MODULE{$module})) {
-		my $void_handler = $MODULE{$module}{void};
+	my ($Chakora::MODULE) = @_;
+	svsflog("chakora", "[MODULES] ".$Chakora::MODULE.": Attempting to unload module. . .");
+	if (defined($Chakora::MODULE{$Chakora::MODULE})) {
+		my $void_handler = $Chakora::MODULE{$Chakora::MODULE}{void};
 		eval
         {
             &{ $void_handler }();
             delete_sub $void_handler;
-            print("[MODULES] ".$module.": Module successfully unloaded.\n");
-            svsflog("chakora", "[MODULES] ".$module.": Module successfully unloaded.");
-			delete $MODULE{$module};
+            print("[MODULES] ".$Chakora::MODULE.": Module successfully unloaded.\n");
+            svsflog("chakora", "[MODULES] ".$Chakora::MODULE.": Module successfully unloaded.");
+			delete $Chakora::MODULE{$Chakora::MODULE};
             return "MODUNLOAD_SUCCESS";
 			1;	
-        } or print("[MODULES] ".$module.": Module failed to unload.\n") and svsflog("chakora", "[MODULES] ".$module.": Module failed to unload.") and return "MODUNLOAD_FAIL";
+        } or print("[MODULES] ".$Chakora::MODULE.": Module failed to unload.\n") and svsflog("chakora", "[MODULES] ".$Chakora::MODULE.": Module failed to unload.") and return "MODUNLOAD_FAIL";
 	} else {
-		print("[MODULES] ".$module.": Module failed to unload. No such module?\n");
-		svsflog("chakora", "[MODULES] ".$module.": Module failed to unload. No such module?");
+		print("[MODULES] ".$Chakora::MODULE.": Module failed to unload. No such module?\n");
+		svsflog("chakora", "[MODULES] ".$Chakora::MODULE.": Module failed to unload. No such module?");
 		return "MODUNLOAD_NOEXIST";
 	}	
 }
@@ -66,15 +66,15 @@ sub module_void {
 sub cmd_add {
     my ($name, $shelp, $fhelp, $handler) = @_;
     my @rname = split('/', $name);
-    $COMMANDS{$rname[0]}{$rname[1]}{name} = $name;
-    $COMMANDS{$rname[0]}{$rname[1]}{handler} = $handler;
-    $HELP{$name}{shelp} = $shelp;
-    $HELP{$name}{fhelp} = $fhelp;
+    $Chakora::COMMANDS{$rname[0]}{$rname[1]}{name} = $name;
+    $Chakora::COMMANDS{$rname[0]}{$rname[1]}{handler} = $handler;
+    $Chakora::HELP{$name}{shelp} = $shelp;
+    $Chakora::HELP{$name}{fhelp} = $fhelp;
 }
 
 sub cmd_del {
 	my ($cmd) = @_;
 	my @scmd = split('/', $cmd);
-	undef $COMMANDS{$scmd[0]}{$scmd[1]};
-	undef $HELP{$cmd};
+	undef $Chakora::COMMANDS{$scmd[0]}{$scmd[1]};
+	undef $Chakora::HELP{$cmd};
 }
