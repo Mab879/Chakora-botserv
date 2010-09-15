91cfbde2f5f86d22b526294cfcebcbe95d56bb4e
Matthew Barksdale <mattwb65@gmail.com>
2010-09-15 19:16:46 -0400

    Oops

--- modules/chanserv/op.pm
+++ modules/chanserv/op.pm
@@ -12,7 +12,7 @@ sub init_cs_op {
 		module_load("chanserv/main");
 	}
 	cmd_add("chanserv/op", "Ops you or another user on a channel.", "OP will allow you to either op yourself\nor another user in a channel that you\nhave the +o flag in.\nSyntax: OP <#channel> [user]", \&svs_cs_op);
-	cmd_add("chanserv/deop", "Deop's you or another user on a channel.", "DEOP will allow you to either deop\nyourself or another user in a channel\nthat you have the +o flag in.\nSyntax: DEOP <#channel> [user]", \&svs_cs_deop);
+	cmd_add("chanserv/deop", "Deops you or another user on a channel.", "DEOP will allow you to either deop\nyourself or another user in a channel\nthat you have the +o flag in.\nSyntax: DEOP <#channel> [user]", \&svs_cs_deop);
         if (!flag_exists("o")) {
                 flaglist_add("o", "Allows the use of the OP/DEOP command");
         }
