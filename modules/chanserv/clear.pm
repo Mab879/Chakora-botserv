b792aa17b847745abccfecc8ac92f2cd4d38cc18
Matthew Barksdale <mattwb65@gmail.com>
2010-09-30 05:15:39 -0400

    Added chanserv/clear, still lacks clear bans, will be added to TODO

--- modules/chanserv/clear.pm
+++ modules/chanserv/clear.pm
@@ -1,4 +1,4 @@
-# chanserv/invite by The Chakora Project. Adds an INVITE command to ChanServ for inviting themselves or others.
+# chanserv/clear by The Chakora Project. Adds a CLEAR command to ChanServ.
 #
 # Copyright (c) 2010 The Chakora Project. All rights reserved.
 # Released under The BSD License (docs/LICENSE - http://www.opensource.org/licenses/bsd-license.php)
@@ -11,7 +11,7 @@ sub init_cs_clear {
 	if (!module_exists("chanserv/main")) {
 		module_load("chanserv/main");
 	}
-	cmd_add("chanserv/clear", "Clears certain channel aspects.", "CLEAR will allow you to clear certain things\nCLEAR USERS for one, can be useful if you are closing your channel.\n[T]\nSyntax: CLEAR <users|flags|bans> <#channel>", \&svs_cs_clear);
+	cmd_add("chanserv/clear", "Clears certain channel aspects.", "CLEAR will allow you to clear certain things\nCLEAR USERS for one, can be useful if you are closing your channel.\n[T]\nSyntax: CLEAR <#channel> <users|flags|bans>", \&svs_cs_clear);
         if (!flag_exists("C")) {
                 flaglist_add("C", "Allows the use of the CLEAR command");
         }
@@ -35,11 +35,11 @@ sub svs_cs_clear {
 		return;
 	}
 	if (!defined($sargv[1]) or !defined($sargv[2])) {
-		serv_notice("chanserv", $user, "Not enough parameters. Syntax: CLEAR <users|flags|bans> <#channel>");
+		serv_notice("chanserv", $user, "Not enough parameters. Syntax: CLEAR <#channel> <users|flags|bans>");
 		return;
 	}
 	my $acc = uidInfo($user, 9);
-	my $chan = $sargv[2];
+	my $chan = $sargv[1];
 	if (!is_registered(2, $chan)) {
 		serv_notice("chanserv", $user, "Channel \002$chan\002 is not registered.");
 		return;
@@ -49,17 +49,24 @@ sub svs_cs_clear {
 		return;
 	}
 	if (!defined $Chakora::channel{lc($chan)}{'members'}) {
-		if (lc($sargv[1]) eq 'users' or lc($sargv[1]) eq 'bans') {
+		if (lc($sargv[2]) eq 'users' or lc($sargv[2]) eq 'bans') {
 			serv_notice("chanserv", $user, "Channel \002$chan\002 is currently empty.");
 			return;
 		}
 	}
 	
-	if (lc($sargv[1]) eq 'users') {
+	if (lc($sargv[2]) eq 'users') {
 		cs_clearusers($chan, $user);
 		svsilog("chanserv", $user, "CLEAR:USERS", $chan);
 		svsflog('commands', uidInfo($user, 1).": ChanServ: CLEAR: USERS: $chan");
 	}
+
+        if (lc($sargv[2]) eq 'flags') {
+                cs_clearflags($chan, $user);
+                svsilog("chanserv", $user, "CLEAR:FLAGS", $chan);
+                svsflog('commands', uidInfo($user, 1).": ChanServ: CLEAR: FLAGS: $chan");
+        }
+
 }
 
 
@@ -68,5 +75,16 @@ sub cs_clearusers {
 	my @cusers = split(' ', $Chakora::channel{lc($c)}{'members'});
 	foreach my $user (@cusers) {
 		serv_kick("chanserv", $c, $user, "CLEAR USERS by ".uidInfo($u, 1));
+	}
+}
+
+sub cs_clearflags {
+	my ($c, $u) = @_;
+	foreach my $key (keys %Chakora::DB_chanflags) {
+		if (lc($Chakora::DB_chanflags{$key}{chan}) eq lc($c)) {
+			if(lc($Chakora::DB_chan{$c}{founder}) ne lc($Chakora::DB_chanflags{$key}{account})) {
+				delete $Chakora::DB_chanflags{$key};
+			}
+		}
 	}
 }
