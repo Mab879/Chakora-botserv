f7465663e490804f8c9bbbdf66dc28ba06d1d6c4
Matthew Barksdale <mattwb65@gmail.com>
2010-09-30 05:16:10 -0400

    C should be a default flag upon registration

--- modules/chanserv/register.pm
+++ modules/chanserv/register.pm
@@ -47,7 +47,7 @@ sub svs_cs_register {
 	
 	my $chan = $sargv[1];
 	
-	my $flags = '+vVoOtskiRmFSL';
+	my $flags = '+vVoOtskiRmFSLC';
 	if (defined $Chakora::PROTO_SETTINGS{owner}) {
 		$flags .= 'qQ';
 	}
