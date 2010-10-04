746cd1a79ca64c1f23a647f0cd4231460d3aec85
Russell Bradford <russell@surgehost.net>
2010-10-04 15:22:39 +0100

    Opers can now use chanserv/mode regardless of what flags they have on the channel providing 'chanserv:can_override' is set for the operators oper class in the services config (soper.conf). The Channel also Recieves a notice when an operator over-rides using chanserv/mode

--- contrib/russellb28/cs_mode.pm
+++ contrib/russellb28/cs_mode.pm
@@ -1,4 +1,4 @@
-# chanserv/mode by Russell Bradford. Adds MODE to ChanServ, which allows users with the 's' flag to set modes on the channel via ChanServ
+# chanserv/speak by Russell Bradford. Adds MODE to ChanServ, which allows users with the 's' flag to set modes on the channel via ChanServ
 #
 # Copyright (c) 2010 The Chakora Project. All rights reserved.
 # Released under The BSD License (docs/LICENSE - http://www.opensource.org/licenses/bsd-license.php)
@@ -40,17 +40,12 @@ sub svs_cs_mode {
 	}
 
 	if (!defined($sargv[2])) {
-		serv_notice("chanserv", $user, "Not enough parameters. Syntax: SAY <channel> [+/- modes]");
+		serv_notice("chanserv", $user, "Not enough parameters. Syntax: MODE <channel> [+/- modes]");
 		return;
 	}
 
 	if (!is_registered(2, $sargv[1])) {
 		serv_notice("chanserv", $user, "Channel \002$sargv[1]\002 is not registered.");
-		return;
-	}
-
-	elsif (!has_flag(uidInfo($user, 9), $sargv[1], "s")) {
-		serv_notice("chanserv", $user, "Permission denied");
 		return;
 	}
 
@@ -61,11 +56,37 @@ sub svs_cs_mode {
        $vars = $sargv[2];
 	for ($i = 3; $i < count(@sargv); $i++) { $vars .= ' '.$sargv[$i]; }
 
-	svsilog("chanserv", $user, "MODE", $sargv[1]." ".$vars);
-	svsflog('commands', uidInfo($user, 1).": ChanServ: MODE: $sargv[1] $vars");
-	$dele .= 'serv_cmode("chanserv", "'.$sargv[1].'", "'.$vars.'"); ';
-	$dele .= '1; ';
-	eval($dele) or svsilog("chanserv", $user, "MODE:FAIL", $@) and svsflog('commands', uidInfo($user, 1)." ChanServ: MODE:FAIL: ".$@) and serv_notice("chanserv", $user, "An error occurred. Please report this to an IRCop immediately.")
+
+	if (has_spower($user, 'chanserv:can_override')) 
+	{
+		$dele .= 'serv_cmode("chanserv", "'.$sargv[1].'", "'.$vars.'"); ';
+		$dele .= '1; ';
+		eval($dele);
+		if (!has_flag(uidInfo($user, 9), $sargv[1], "s")) {
+			svsilog("chanserv", $user, "MODE", $sargv[1]." ".$vars." (OVER-RIDE)");
+			svsflog('commands', uidInfo($user, 1).": ChanServ: MODE: $sargv[1] $vars  (OVER-RIDE)");
+			serv_notice("chanserv", $sargv[1], "(OVER-RIDE) ".uidInfo($user, 1)." set modes ".$vars." via ChanServ");
+		}
+		else
+		{
+			svsilog("chanserv", $user, "MODE", $sargv[1]." ".$vars);
+			svsflog('commands', uidInfo($user, 1).": ChanServ: MODE: $sargv[1] $vars");
+		}
+		return;
+	}
+	elsif(!has_spower($user, 'chanserv:can_override'))
+	{
+		if (!has_flag(uidInfo($user, 9), $sargv[1], "s")) {
+			serv_notice("chanserv", $user, "Permission denied");
+			return;
+		}
+		svsilog("chanserv", $user, "MODE", $sargv[1]." ".$vars);
+		svsflog('commands', uidInfo($user, 1).": ChanServ: MODE: $sargv[1] $vars");
+		$dele .= 'serv_cmode("chanserv", "'.$sargv[1].'", "'.$vars.'"); ';
+		$dele .= '1; ';
+		eval($dele) or svsilog("chanserv", $user, "MODE:FAIL", $@) and svsflog('commands', uidInfo($user, 1)." ChanServ: MODE:FAIL: ".$@) and serv_notice("chanserv", $user, "An error occurred. Please report this to an IRCop immediately.");
+		return;
+	}
 }
 
 1;
