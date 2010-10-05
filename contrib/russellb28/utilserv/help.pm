0ead7b10a296adf24e4d0ea0d5277c4a7ede1128
Russell Bradford <russell@surgehost.net>
2010-10-05 23:51:54 +0100

    Added UtilServ Service which includes the previously removed operserv/dns and a nice readme!

---
+++ contrib/russellb28/utilserv/help.pm
@@ -0,0 +1,72 @@
+# hostserv/help by Russell Bradford. Adds help functions to UtilServ.
+#
+# Copyright (c) 2010 The Chakora Project. All rights reserved.
+# Released under The BSD License (docs/LICENSE - http://www.opensource.org/licenses/bsd-license.php)
+use strict;
+use warnings;
+
+module_init("utilserv/help", "Russell Bradford", "0.1", \&init_us_help, \&void_us_help, "all");
+
+sub init_us_help {
+	if (!module_exists("utilserv/main")) {
+		module_load("utilserv/main");
+	}
+	cmd_add("utilserv/help", "NO_HELP_ENTRY", "NO_HELP_ENTRY", \&svs_us_help);
+}
+
+sub void_us_help {
+	delete_sub 'init_us_help';
+	delete_sub 'svs_us_help';
+	cmd_del("utilserv/help");
+	delete_sub 'void_us_help';
+}
+
+sub svs_us_help {
+    my ($user, @sargv) = @_;
+	if (defined($sargv[1])) {
+		my $hcmd = "utilserv/".lc($sargv[1]);
+		if (defined($Chakora::HELP{$hcmd}{fhelp}) and $Chakora::HELP{$hcmd}{fhelp} ne "NO_HELP_ENTRY") {
+			my @fhelp = split('\n', $Chakora::HELP{$hcmd}{fhelp});
+			my ($help);
+			serv_notice("utilserv", $user, "\002***** UtilServ Help *****\002");
+			serv_notice("utilserv", $user, "Help for \002".uc($sargv[1])."\002:");
+			serv_notice("utilserv", $user, "\002\002");
+			foreach $help (@fhelp) {
+				$help =~ s/\[T\]/     /g;
+				serv_notice("utilserv", $user, $help);
+			}
+			serv_notice("utilserv", $user, "\002\002");
+			serv_notice("utilserv", $user, "\002***** End of Help *****\002");
+		} else {
+			serv_notice("utilserv", $user, "No help available for \002".uc($sargv[1])."\002.");
+		}
+	} else {
+		serv_notice("utilserv", $user, "\002***** UtilServ Help *****\002");
+		serv_notice("utilserv", $user, "\002UtilServ\002 allows users to use a variety of non irc related");
+		serv_notice("utilserv", $user, "commands to do simple tasks such as looking up dns records, getting");
+		serv_notice("utilserv", $user, "the local time from our server and many more useful features.");
+		serv_notice("utilserv", $user, "\002\002");
+		serv_notice("utilserv", $user, "For more information on a command, please type:");
+		serv_notice("utilserv", $user, "\002/msg ".$Chakora::svsnick{'utilserv'}." HELP <command>\002");
+		serv_notice("utilserv", $user, "\002\002");
+		serv_notice("utilserv", $user, "The following commands are available:");
+		my %commands = %Chakora::HELP;
+		my ($calc, $dv);
+		foreach my $key (sort keys %commands) {
+			my @skey = split('/', $key);
+			if (lc($skey[0]) eq 'utilserv') {
+				unless ($commands{$key}{shelp} eq "NO_HELP_ENTRY" or length($key) > 23) {
+					$calc = length($key);
+					$dv = "";
+					while ($calc != 25) {
+						$dv .= ' ';
+						$calc += 1;
+					}
+					serv_notice("utilserv", $user, "   \002".uc($skey[1])."\002".$dv.$commands{$key}{shelp});
+				}
+			}
+		}
+		serv_notice("utilserv", $user, "\002\002");
+		serv_notice("utilserv", $user, "\002***** End of Help *****\002");
+	}
+}
---
+++ contrib/russellb28/utilserv/main.pm
@@ -0,0 +1,59 @@
+# utilserv/main by Russell Bradford. Creates the UtilServ service.
+#
+# Copyright (c) 2010 The Chakora Project. All rights reserved.
+# Released under The BSD License (docs/LICENSE - http://www.opensource.org/licenses/bsd-license.php)
+use strict;
+use warnings;
+
+module_init("utilserv/main", "Russell Bradford", "0.1", \&init_us_main, \&void_us_main, "all");
+
+sub init_us_main {
+	hook_kill_add(\&ircd_us_kill);
+	create_cmdtree("utilserv");
+	if (!$Chakora::synced) { hook_pds_add(\&svs_us_main); }
+	else { svs_us_main(); }
+}
+
+sub void_us_main {
+	delete_sub 'init_us_main';
+	delete_sub 'svs_us_main';
+	hook_pds_del(\&svs_us_main);
+	serv_del('UtilServ');
+	delete_cmdtree("utilserv");
+	hook_kill_del(\&ircd_us_kill);
+	delete_sub 'ircd_us_kill';
+	delete_sub 'void_us_main';
+}
+
+sub svs_us_main {
+	if (!config('utilserv', 'nick')) {
+		svsflog('modules', 'Unable to create UtilServ. utilserv:nick is not defined in the config!');
+		if ($Chakora::synced) { logchan('operserv', "\002UtilServ\002: Unable to create UtilServ. utilserv:nick is not defined in the config!"); }
+		module_void("utilserv/main");
+	} elsif (!config('utilserv', 'user')) {
+		svsflog('modules', 'Unable to create UtilServ. utilserv:user is not defined in the config!');
+		if ($Chakora::synced) { logchan('operserv', "\002UtilServ\002: Unable to create UtilServ. utilserv:user is not defined in the config!"); }
+		module_void("utilserv/main");
+	} elsif (!config('utilserv', 'host')) {
+		svsflog('modules', 'Unable to create UtilServ. utilserv:host is not defined in the config!');
+		if ($Chakora::synced) { logchan('operserv', "\002UtilServ\002: Unable to create UtilServ. utilserv:host is not defined in the config!"); }
+		module_void("utilserv/main");
+	} elsif (!config('utilserv', 'real')) {
+		svsflog('modules', 'Unable to create UtilServ. utilserv:real is not defined in the config!');
+		if ($Chakora::synced) { logchan('operserv', "\002UtilServ\002: Unable to create UtilServ. utilserv:real is not defined in the config!"); }
+		module_void("utilserv/main");
+	} else {
+		my $modes = '+io';
+		if (defined $Chakora::PROTO_SETTINGS{god}) { $modes .= $Chakora::PROTO_SETTINGS{god}; }
+		serv_add('utilserv', config('utilserv', 'user'), config('utilserv', 'nick'), config('utilserv', 'host'), $modes, config('utilserv', 'real'));
+	}
+}
+
+sub ircd_us_kill {
+	my ($user, $target, $reason) = @_;
+	
+	if ($target eq $Chakora::svsuid{'utilserv'}) {
+		serv_del("UtilServ");
+		ircd_us_main();
+	}
+}
