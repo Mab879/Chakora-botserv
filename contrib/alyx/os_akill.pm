609592ff1ed316a5d0bd29a49d91a0eae3d74227
Alyx Rothschild <chazz@staticbox.net>
2010-10-08 00:25:42 -0500

    Hopefully, the user would be banned anyways, but idgaf

--- contrib/alyx/os_akill.pm
+++ contrib/alyx/os_akill.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # os_akill.pm - an AKILL module for Chakora
-# Copyright 2010 Alyx Rothschild and Woomoo Development Group <alyx@woomoo.org>
+# Copyright 2010 Woomoo Development Group <http://woomoo.org>
 # Released under the simplified BSD license
 
 use strict;
@@ -11,22 +11,27 @@ use Net::DNS;
 use Config::JSON;
 use constant { PACKAGE_VER => '0.1.0 devel' };
 
-our $pt = new Net::Patricia; #Create the Patricia Trie
+our $pt = new Net::Patricia;    #Create the Patricia Trie
 
 module_init( "operserv/akill",
     "Woomoo Development Group <http://dev.woomoo.org",
     PACKAGE_VER, \&modinit, \&moddeinit, "all" );
 
 sub modinit() {
-	hook_uid_add(\&handle_connection);
-	cmd_add("operserv/akill", "Manages network bans.", "Manages network bans.", \&os_cmd_akill);
+    hook_uid_add( \&handle_connection );
+    cmd_add(
+        "operserv/akill",        "Manages network bans.",
+        "Manages network bans.", \&os_cmd_akill
+    );
 }
 
-sub handle_connection
-{
+sub handle_connection {
+    my ( $uid, $nick, $user, $host, $mask, $ip, $server ) = @_;
+	if ($pt->match_string($ip)) {
+		serv_kill("operserv", $uid, "Connection from banned netmask.");
+		
 }
 
-sub os_cmd_akill
-{
+sub os_cmd_akill {
 }
 
