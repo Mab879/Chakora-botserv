04c543c619e01b269fe9f52d5bc1c992795f4a86
Matthew Barksdale <mattwb65@gmail.com>
2010-11-11 23:50:07 -0500

    Basic operserv/stats

---
+++ modules/operserv/stats.pm
@@ -0,0 +1,42 @@
+# operserv/stats by The Chakora Project. Adds STATS to OperServ, will return services stats.
+#
+# Copyright (c) 2010 The Chakora Project. All rights reserved.
+# This software is free software; rights to this code are stated in docs/LICENSE.
+use strict;
+use warnings;
+
+module_init("operserv/stats", "The Chakora Project", "0.1", \&init_os_stats, \&void_os_stats);
+
+sub init_os_stats {
+	cmd_add("operserv/stats", "Display services uptime.", "Display when services were started, and how long they've been running.", \&svs_os_stats);
+}
+
+sub void_os_stats {
+	delete_sub 'init_os_stats';
+	delete_sub 'svs_os_stats';
+	cmd_del("operserv/stats");
+	delete_sub 'void_os_stats';
+}
+
+sub svs_os_stats {
+    	my ($user, @sargv) = @_;
+	my $nicks = 0;
+	my $accounts = 0;
+	my $channels = 0;
+	foreach my $nick (keys %Chakora::DB_NICK) { 
+		$nicks++;
+    	}
+ 	foreach my $acc (keys %Chakora::DB_ACCOUNT) {
+		$accounts++;
+	}
+	foreach my $chan (keys %Chakora::DB_CHAN) {	
+		$channels++;
+	}
+	
+	serv_notice("operserv", $user, "-Services stats-");
+	serv_notice("operserv", $user, "Registered accounts: ".scalar(keys %Chakora::DB_account));
+	serv_notice("operserv", $user, "Registered nicknames: ".scalar(keys %Chakora::DB_nick));
+	serv_notice("operserv", $user, "Registered channels: ".scalar(keys %Chakora::DB_chan));
+}
+
+1;
