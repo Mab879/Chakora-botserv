5a86cd82c044991eb5c5c73261a9d5c4af539f02
Matthew Barksdale <mattwb65@gmail.com>
2010-11-15 17:32:26 +0800

    Added fantasy_del to remove the fantasy handler for a command when its unloaded

--- modules/chanserv/main.pm
+++ modules/chanserv/main.pm
@@ -209,7 +209,13 @@ sub fantasy {
 		return 0;
 	}
 }
-                
+ 
+sub fantasy_del {
+        my ($cmd) = @_;
+        delete $Chakora::FANTASY{lc($cmd)};
+        return 1;
+}
+               
 sub ircd_cs_ns_id {
 	my ($user, $account) = @_;
 	if (defined($Chakora::uid{$user}{'chans'})) {
