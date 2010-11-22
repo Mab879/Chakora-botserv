2652114fc2a23d6c7103219fb6f7ab5eabdaa9e4
Matthew Burket <Matthew@assignitapp.com>
2010-11-22 16:33:16 -0600

    A bit more work on the checknick.pm

--- modules/infoserv/checknick.pm
+++ modules/infoserv/checknick.pm
@@ -19,10 +19,14 @@ sub svs_is_checknick {
 	 (!defined($sargv[1])) {
 	          serv_notice("infoserv", $user, "Not enough parameters. Syntax: CHECKNICK <nick>.");
 			      }
-	 if (is_identified)$user)) {
+	 if (is_identified)$user) {
                   serv_notice("infoserv", $user, "Your nick, /002$user/002 is registerd and idnetifyied.");
 	}	  
 
-         if (is_identified($sargv[1])) {
-                  serv_notice("infoserv
+         if (defined($Chakora::DB_nick{lc(uidInfo($sargv[1], 1))}{account}) {
+                  serv_notice("infoserv", $user, "The nick /002$sargv[1]/002 is registred.");
+		}
+         if (!defined($Chakora::DB_nick{lc(uidInfo($sargv[1], 1))}{account})) {
+}                  serv_notice("infoserv", $user, "The user /002$sargv[1]/002 is not registed.");
+}
   
