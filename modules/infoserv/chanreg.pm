5c88f4ff293f22bb12820c3b734c12215e6ad7be
Matthew Burket <mab879@msn.com>
2010-11-21 21:37:42 -0600

    Added channeling droping to infoserv/chanreg.

--- modules/infoserv/chanreg.pm
+++ modules/infoserv/chanreg.pm
@@ -3,6 +3,17 @@ use strict;
 use warnings;
 
 #Start The module
-	module_init("infoserv/chanreg", "Franklin IRC Services", "0.1", \&inti_is_chanreg \&void_is_main);
+	module_init("infoserv/chanreg", "Franklin IRC Services", "0.1", \&inti_is_chanreg \&void_is_chanreg);
 #Add the command
+sub inti_is_chanreg {
 	cmd_add("infoserv/chanreg", "Shows how to register a channel.", \&svs_is_chanreg);
+	cmd_add("infoserv/chandrop", "Shows how to drop a channel", \&svs_is_chandrop);
+}
+
+sub void_is_chanreg {
+	delete_sub 'inti_is_chanreg';
+	delete_sub 'svs_is_chanreg';
+	delete_sub 'svs_is)chandrop';
+}
+
+	
