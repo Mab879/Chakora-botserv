8ba6c46cdbb28aa490d523606f15a43a33b4cf00
Matthew Burket <mab879@msn.com>
2010-11-21 20:03:35 -0600

    Made is so if meta infoserv is set infoserv will join to that channel. Also I it follows ChanServ Patterns of joining and parting

--- modules/infoserv/main.pm
+++ modules/infoserv/main.pm
@@ -14,7 +14,6 @@ sub inti_is_main {
 	create_cmdtree("infoserv");
 	hook_join_add(\&ircd_is_join);
 	hook_part_add(\&ircd_is_part);
-	hook_quit_add(\&ircd_is_quit);
 	hook_privmsg_add(\&ircd_is_quit);
 	if (!$Chakora::Synced) { 
 		hook_pds_add(\&ircd_is_main);
@@ -58,7 +57,64 @@ sub ircd_is_main {
 			$modes, config( 'infoserv', 'real' )
 		);	
 }
-sub ircd_is_join
-	my ($userm $chan) = !_;
+sub inti_is_join {
+	my @cmems = split(' ', $Chakora::channel{lc($chan)}{'members'});
+	if (count(@cmems) == 1) {
+		if (defined($Chakora::DB_chan{lc($chan)}{name}) and metadata(2, $chan, 'option:infoserv') and lc($chan) ne lc(config('log', 'logchan'))) {
+			serv_join("infoserv", $chan);
+		}
+	}
+	apply_status($user, $chan);
+}
 
+sub ircd_is_part {
+	my ($user, $chan, undef) = @_;
+	
+	my @cmems = split(' ', $Chakora::channel{lc($chan)}{'members'});
+	if (count(@cmems) < 1 and defined($Chakora::DB_chan{lc($chan)}{name}) and metadata(2, $chan, 'option:infoserv') and lc($chan) ne lc(config('log', 'logchan'))) {
+		serv_part("infoserv", $chan, "Channel user count has dropped below 1.");
+	}
+}
+
+sub ircd_is_quit {
+	my ($user, undef) = @_;
+
+	my @chns = split(' ', uidInfo($user, 10));
+	foreach my $chn (@chns) {
+		my @cmems = split(' ', $Chakora::channel{lc($chn)}{'members'});
+		if (count(@cmems) < 1 and defined($Chakora::DB_chan{lc($chn)}{name}) and metadata(2, $chn, 'option:infoserv') and lc($chn) ne lc(config('log', 'logchan'))) {
+			serv_part("infoserv", $chn, "Channel user count has dropped below 1.");
+		}
+	}
+}
+
+sub ircd_is_privmsg {
+	my ($user, $target, $msg) = @_;
+	
+	my @rex = split(' ', $msg);
+	if (substr($target, 0, 1) eq '#') {
+		if (defined $Chakora::DB_chan{lc($target)}{name}) {
+			if (metadata(2, $target, 'option:fantasy')) {
+				unless (!config('chanserv', 'fantasy_char')) {
+					if (substr($rex[0], 0, 1) eq config('chanserv', 'fantasy_char')) {
+						my $ecmd = substr($rex[0], 1);
+						if (defined $Chakora::COMMANDS{'chanserv'}{lc($ecmd)} and defined $Chakora::FANTASY{lc($ecmd)}) {
+							my $sub_ref = $Chakora::COMMANDS{'chanserv'}{lc($ecmd)}{handler};
+							my (@bargv);
+							if ($Chakora::FANTASY{lc($ecmd)} == 0) {
+								for (my $i = 1; $i < count(@rex); $i++) { $bargv[$i] = $rex[$i]; }
+							}
+							elsif ($Chakora::FANTASY{lc($ecmd)} == 1)  {
+								push(@bargv, "");
+								push(@bargv, $target);
+								for (my $i = 1; $i < count(@rex); $i++) { push(@bargv, $rex[$i]); }
+							}
+							
+							eval { &{$sub_ref}($user, @bargv); };
+						}
+					}
+                }
+			}
+		}
+	}
 }
