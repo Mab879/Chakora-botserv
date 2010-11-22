c73e92c7552d187f7e52dda633d441a246155aec
Matthew Burket <Matthew@assignitapp.com>
2010-11-22 09:38:19 -0600

    A bit more work on infoserv/chanreg.

--- modules/infoserv/chanreg.pm
+++ modules/infoserv/chanreg.pm
@@ -1,4 +1,7 @@
 # infoserv/chanreg by Franklin IRC Services. Tells how to register a channel.
+#
+# Copyright (c) 2010 The Chakora Project. All rights reserved.
+# This software is free software; rights to this code are stated in docs/LICENSE.
 use strict;
 use warnings;
 
@@ -13,7 +16,15 @@ sub inti_is_chanreg {
 sub void_is_chanreg {
 	delete_sub 'inti_is_chanreg';
 	delete_sub 'svs_is_chanreg';
-	delete_sub 'svs_is)chandrop';
+	delete_sub 'svs_is_chandrop';
+	delete_sub 'void_is_chanreg';
 }
+sub svs_is_chanref {
+	my ($user, @sargv) = @_;
+	serv_notice("infoserv", $user, "To register a channel first have to have a registred nickname. If don't if have one plese type:/n /msg infoserv nickcheck $user");
+}
+
+1;
+  
 
 	
