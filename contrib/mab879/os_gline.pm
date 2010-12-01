390e662089bee7feb10ff8f40a9de9781911c5cc
Matthew Burket <Matthew@assignitapp.com>
2010-11-30 21:05:31 -0600

    [operserv/gline] More work.

--- contrib/mab879/os_gline.pm
+++ contrib/mab879/os_gline.pm
@@ -9,9 +9,9 @@ use warnings;
 	module_init("operserv/gline" "Mathew Burket" "0.1" \&init_os_gline, \&void_os_gline);
 
 sub init_os_gline {
-	cmd_add("operserv/gline" "Set a global ban or gline a user via operserv." "GLINE allows an IRC operator to issue a Network Ban for a user./n With a reason your nick is hidden with a reason./n With out one your nick will show. [T]\nSyntax: GLINE <nickane/ ident / hostname> [duration] [reason] " \&svs_os_gline);
-	cmd_add("operserv/kline" "Sets a global ban." "KLINE allows an IRC operator to issue a Network Ban for a user./n With a reason your nick is hidden with a reason./n With out one your nick will show. [T]\nSyntax: KLINE <nickane/ ident / hostname> [duration] [reason] " \&svs_os_kline); 
-	cmd_add("operserv/netban" "Sets a global ban" "NETBAN allows an IRC operator to issue a Network Ban for a user./n With a reason your nick is hidden with a reason./n With out one your nick will show. [T]\nSyntax: GLINE <nickane/ ident / hostname> [duration] [reason] " \&svs_os_netban);
+	cmd_add("operserv/gline", "Set a network ban..", "GLINE allows an IRC operator to issue a Network Ban for a user./n With a reason your nick is hidden with a reason./n With out one your nick will show. [T]\nSyntax: GLINE <nickane/ ident / hostname> [duration] [reason] " \&svs_os_gline);
+	cmd_add("operserv/kline","Sets a global ban.", "KLINE allows an IRC operator", "to issue a Network Ban for a user./n With a reason your nick is hidden with a reason./n With out one your nick will show. [T]\nSyntax: KLINE <nickane/ ident / hostname> [duration] [reason]" \&svs_os_kline); 
+	cmd_add("operserv/netban","Sets a global ban", "NETBAN allows an IRC operator to issue a Network Ban for a user./n With a reason your nick is hidden with a reason./n With out one your nick will show. [T]\nSyntax: NETBAN <nickane/ ident / hostname> [duration] [reason] " \&svs_os_netban);
 	
 }
 
@@ -19,6 +19,8 @@ sub void_os_gline {
 	sub_delete "init_os_gline";
 	sub_delete "init_os_gline";
 	sub_delete "svs_os_gline";
+	sub_delete "svs_os_kline";
+	sub delete "svs_os_netban"
 }
 
 sub svs_os_gline {
@@ -33,12 +35,15 @@ if (has_spower($user, 'operserv:gline'))	(
 		return;
 	}
 	if (!defined($sargv[3])) {
-		net_ban($sargv[1] $sargv[2] "You have been banned by:" $user );
-		svsilog("operserv", $user "GLINE: $sargv");
-		serv_notice("operserv", $user "PLEASE NOTE: That work will be the reason for this Network Ban./n Added gline:" $sargv);
+		net_ban($sargv $user );
+		svsilog("operserv", $user "GLINE:" $sargv);
+		serv_notice("operserv", $user "PLEASE NOTE: That your nick  was the  reason for this Network Ban./n Added gline:" $sargv);
+		return;
 	}
 	if (defined($sargv[3])) 
-		net_ban()
+		net_ban($sargv);
+		svsilog("operserv", $user GLINE: $sargv);
+		serv_ntoice("operserv", $user, $sargv); 
 		
 	
 		)
@@ -49,3 +54,33 @@ if (has_spower($user, 'operserv:gline'))	(
 		
 	}
 
+sub svs_os_kline {
+	my ($user, @sargv) = @_;
+if (has_spower($user, 'operserv:gline'))	(
+	if (!defined($sargv[1])) (
+		serv_notice("operserv", $user, "Not enough parameters. Syntax: KLINE <nick / ident / hostname> [durtion] [reason]");
+		return;
+		)
+	if (!defined($sargv[2])) {
+		serv_notice("operserv", $user "Not enough paramenters. Syntax: KLINE <nick / ident / hostname> [duriton] [reason]");
+		return;
+	}
+	if (!defined($sargv[3])) {
+		net_ban($sargv $user );
+		svsilog("operserv", $user "GLINE:" $sargv);
+		serv_notice("operserv", $user "PLEASE NOTE: That your nick  was the  reason for this Network Ban./n Added kline:" $sargv);
+		return;
+	}
+	if (defined($sargv[3])) 
+		net_ban($sargv);
+		svsilog("operserv", $user KLINE: $sargv);
+		serv_ntoice("operserv", $user, $sargv); 
+		
+	
+		)
+	else  {
+		serv_notice("operserv", $user "You do not have access to the Kline command!/n YOUR ACTION HAS BEEN LOGGED!");
+		svsilog("operserv", $user, "Kline: $sargv");
+	}
+		
+	}
