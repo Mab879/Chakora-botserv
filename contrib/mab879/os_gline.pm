f86c1a85a0b7dc5f88d4ec1311c7559780a09820
Matthew Burket <Matthew@assignitapp.com>
2010-12-01 13:45:42 -0600

    [contrib/operserv] Finished up os_gline.

--- contrib/mab879/os_gline.pm
+++ contrib/mab879/os_gline.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 #Start the module
-	module_init("operserv/gline" "Mathew Burket" "0.1" \&init_os_gline, \&void_os_gline);
+	module_init("operserv/gline", "Mathew Burket", "0.1", \&init_os_gline, \&void_os_gline);
 
 sub init_os_gline {
 	cmd_add("operserv/gline", "Set a network ban..", "GLINE allows an IRC operator to issue a Network Ban for a user./n With a reason your nick is hidden with a reason./n With out one your nick will show. [T]\nSyntax: GLINE <nickane/ ident / hostname> [duration] [reason] " \&svs_os_gline);
@@ -67,7 +67,7 @@ if (has_spower($user, 'operserv:gline'))	(
 	}
 	if (!defined($sargv[3])) {
 		net_ban($sargv $user );
-		svsilog("operserv", $user "GLINE:" $sargv);
+		svsilog("operserv", $user "KLINE:" $sargv);
 		serv_notice("operserv", $user "PLEASE NOTE: That your nick  was the  reason for this Network Ban./n Added kline:" $sargv);
 		return;
 	}
@@ -82,5 +82,32 @@ if (has_spower($user, 'operserv:gline'))	(
 		serv_notice("operserv", $user "You do not have access to the Kline command!/n YOUR ACTION HAS BEEN LOGGED!");
 		svsilog("operserv", $user, "Kline: $sargv");
 	}
-		
+sub svs_os_netban {
+	my ($user, @sargv) = @_;
+if (has_spower($user, 'operserv:gline'))	(
+	if (!defined($sargv[1])) (
+		serv_notice("operserv", $user, "Not enough parameters. Syntax: NETBAN <nick / ident / hostname> [durtion] [reason]");
+		return;
+		)
+	if (!defined($sargv[2])) {
+		serv_notice("operserv", $user "Not enough paramenters. Syntax: NETBAN <nick / ident / hostname> [duriton] [reason]");
+		return;
 	}
+	if (!defined($sargv[3])) {
+		net_ban($sargv $user );
+		svsilog("operserv", $user "KLINE:" $sargv);
+		serv_notice("operserv", $user "PLEASE NOTE: That your nick  was the  reason for this Network Ban./n Added a Network Ban for:" $sargv);
+		return;
+	}
+	if (defined($sargv[3])) 
+		net_ban($sargv);
+		svsilog("operserv", $user NETBAN: $sargv);
+		serv_ntoice("operserv", $user, $sargv); 
+		
+	
+		)
+	else  {
+		serv_notice("operserv", $user "You do not have access to the Netban  command!/n YOUR ACTION HAS BEEN LOGGED!");
+		svsilog("operserv", $user, "NETBAN: $sargv");
+	}		
+}
