61d017facd83b9cd886aab8a9e34f431245aa03e
Matthew Burket <Matthew@assignitapp.com>
2010-12-02 14:24:38 -0600

    [operserv/DEFCON] A bit of updated.

--- modules/operserv/DEFCON.pm
+++ modules/operserv/DEFCON.pm
@@ -1,4 +1,4 @@
-# operserv/defcon by Franklin IRC Services. BOTNET attack respose
+# operserv/defcon by Franklin IRC Services. BOTNET attack respose.
 #
 # Copyright (c) 2010 Franklin IRC Services. All rights reserved.
 # This software is free software; rights to this code are stated in docs/LICENSE.
@@ -8,7 +8,7 @@ use warnings;
 #Start The module
 	module_init("operserv/defcon", "Franklin IRC Services", "0.1", \&inti_os_defcon, \&void_os_defcon);
 #Add the command
-sub inti_is_chanreg {
+sub inti_os_defcon {
 	cmd_add("operserv/defcon", "The DEFCON System","Allows opers to lockdown the network", \&svs_os_defcon);
 }
 sub void_os_defcon {
@@ -25,7 +25,12 @@ my ($user, @sargv) = @_;
 		if (defined($sargv[1])) {
 			svsilog("operserv", $user, "DEFCON", "\002".$sargv[1]."\002");
 			svsflog('commands', uidInfo($user, 1).": OperServ: DEFCON: $sargv[1] ");
-			
+			set_defcon($sargv[1], $user);
+			send_global("DEFCON has been set on. The current DEFCON is" $sargv[1]);
+			wall_ops($user "Has set the DEFCON TO" $sargv[1]);
 		}
 	)
+else {
+	serv_notice("operserv", $user, "You don't have access to the DEFCON system.");
+}
 }
