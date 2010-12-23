ed3163095091c3a34013352b679e3955b08f4867
Matthew Burket <Matthew@assignitapp.com>
2010-12-23 14:50:58 -0600

    Started the config checker.

---
+++ contrib/mab879/configreader.pm
@@ -0,0 +1,8 @@
+# Config Checker by Matthew Burket
+#
+# Copyright (c) 2010 Matthew Burket. All rights reserved.
+# This software is free software; rights to this code are stated in docs/LICENSE
+use strict;
+use warnings;
+
+
