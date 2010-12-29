0c1bba7db4199c63d5f758b8f9771dc1f9aed2dd
Matthew Barksdale <mattwb65@gmail.com>
2010-12-30 02:55:18 +0800

    Add base logger, this is where Chakora will request something to be logged

---
+++ src/Logger.pm
@@ -0,0 +1,12 @@
+# Logger by The Chakora Project. Logging functions.
+#
+# Copyright (c) 2010 The Chakora Project. All rights reserved.
+# This software is free software; rights to this code are stated in docs/LICENSE.
+
+use Log::Channel;
+use Log::File;
+require Log::Terminal if $Chakora::IN_DEBUG;
+
+
+
+1;
